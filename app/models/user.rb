7a7106fc114606cdcb15f6f6835ed92b1de87c5c
hungnd <hungnguyenw420@gmail.com>
2020-09-17 15:53:26 +0700

    add validation to users + pwd

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,10 @@
 
 # dasdasda
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
   has_many :microposts
-  validates :name, presence: true
-  validates :email, presence: true
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+  has_secure_password
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,4 +6,57 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @user = User.new(name: 'Example', email: 'example@email.com')
+  end
+
+  test 'should be valid' do
+    assert @user.valid?
+  end
+
+  test 'name should be present' do
+    @user.name = '   '
+    assert_not @user.valid?
+  end
+
+  test 'email should be present' do
+    @user.email = '  '
+    assert_not @user.valid?
+  end
+
+  test 'name length shoud be less than 50' do
+    @user.name = 'a' * 51
+    assert_not @user.valid?
+  end
+
+  test 'valid email validation' do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test 'invalid email validation' do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz..com foo@bar+baz.com]
+    invalid_addresses.each do |valid_address|
+      @user.email = invalid_addresses
+      assert_not @user.valid?, "#{valid_address.inspect} should be invalid"
+    end
+  end
+
+  test 'email should be unique' do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?, duplicate_user.email
+  end
+
+  test 'email addresses should be saved as lower-case' do
+    mixed_case_email = 'Foo@ExAMPle.CoM'
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
 end
