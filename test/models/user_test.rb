de4e7ed9eef1bc6623fcf6623cc0e0bc380ac00f
hungnd <hungnguyenw420@gmail.com>
2020-09-17 16:32:53 +0700

    add password func

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -7,7 +7,8 @@ class UserTest < ActiveSupport::TestCase
   #   assert true
   # end
   def setup
-    @user = User.new(name: 'Example', email: 'example@email.com')
+    @user = User.new(name: 'Example', email: 'example@email.com',
+                     password: 'Hung123456', password_confirmation: 'Hung123456')
   end
 
   test 'should be valid' do
@@ -41,9 +42,9 @@ class UserTest < ActiveSupport::TestCase
   test 'invalid email validation' do
     invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
                            foo@bar_baz..com foo@bar+baz.com]
-    invalid_addresses.each do |valid_address|
-      @user.email = invalid_addresses
-      assert_not @user.valid?, "#{valid_address.inspect} should be invalid"
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
     end
   end
 
@@ -53,10 +54,15 @@ class UserTest < ActiveSupport::TestCase
     assert_not duplicate_user.valid?, duplicate_user.email
   end
 
-  test 'email addresses should be saved as lower-case' do
-    mixed_case_email = 'Foo@ExAMPle.CoM'
-    @user.email = mixed_case_email
-    @user.save
-    assert_equal mixed_case_email.downcase, @user.reload.email
+  test 'password minimum length' do
+    short_password = 'a'
+    @user.password = @user.password_confirmation = short_password
+    assert_not @user.valid?
+  end
+
+  test 'password maximum length' do
+    long_password = 'a' * 59
+    @user.password = @user.password_confirmation = long_password
+    assert_not @user.valid?
   end
 end
