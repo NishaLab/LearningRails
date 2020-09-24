c9f275f8ce1c8985d920af6151e772845d00b7da
hungnd <hungnguyenw420@gmail.com>
2020-09-24 15:59:28 +0700

    add activate account feature

--- app/controllers/account_activation_controller.rb
+++ app/controllers/account_activation_controller.rb
@@ -1,4 +1,17 @@
 # frozen_string_literal: true
 
 class AccountActivationController < ApplicationController
+  def edit
+    user = User.find_by(email: params[:email])
+    if user&.authenticated?(:activation, params[:id]) && !user.activated?
+      user.update_attribute(:activated, true)
+      user.update_attribute(:activated_at, Time.zone.now)
+      log_in user
+      flash[:succes] = 'Account Activated'
+      redirect_to user
+    else
+      flash[:danger] = 'Invalid Activation Link'
+      redirect_to root_url
+    end
+  end
 end
