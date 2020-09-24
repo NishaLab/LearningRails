5529af026ace09d62d11e9c841b1bdd2ea4fe85d
hungnd <hungnguyenw420@gmail.com>
2020-09-24 14:50:56 +0700

    add activation properties to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,11 @@
 
 # dasdasda
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
 
   before_save { self.email = email.downcase }
+  before_create :create_activation_digest
+
   has_many :microposts
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
@@ -34,5 +36,12 @@ class User < ApplicationRecord
 
   def forget
     update_attribute(:remember_digest, nil)
+  end
+
+  private
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
